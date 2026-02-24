1a604d74c8bb2616457e080d7cb95600661b394e
Marciano2o <ST10508875@myemeris.edu.za>
2026-02-24 12:35:10 +0200

    V1

---
+++ src/stopandgo/StopandGo.java
@@ -0,0 +1,31 @@
+
+
+package stopandgo;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class StopandGo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    // This line of code is going to repesent a stop and go sign     
+        
+        System.out.println("    XXXXXXXXXX \t\t    XXXXXXXXXX \n  X           X "
+                + "\t  X            X \n X     STOP    X \t X      GO      X \n  X           X \t  X            X\n    XXXXXXXXXX "
+                + "\t\t    XXXXXXXXXX  \n\tXX \t\t\tXX \n\tXX \t\t\tXX  \n\tXX \t"
+                + "\t\tXX \n\tXX\t\t\tXX  \n\tXX\t\t\tXX \n      XXXXXX "
+                + "\t\t      XXXXXX \n     XXXXXXXX\t\t     XXXXXXXX ");
+        
+    /**
+     * The is repesent a road 
+     */    
+        System.out.println("----------------------------------------------"
+                + " \n----------------------------------------------"); 
+        
+    }
+}
